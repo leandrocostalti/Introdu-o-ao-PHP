47f06bee82f155e66628351ab95e917036afb6b5
Leandro Costa <ltitreinamento@gmail.com>
2020-05-14 08:26:25 -0300

    commit dos arquivos PHP iniciais

---
+++ introphp.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+Comentário de varias linhas
+mostrando texto na tela com PHP
+Criando variáveis
+*/
+$idade = 25;
+$stringIdade = "Minha idade é";
+echo "<h1>$stringIdade $idade Anos</h1>";
+?>
---
+++ operadores.php
@@ -0,0 +1,17 @@
+<?php
+$valorA = 3;
+$valorB = 2;
+//Soma
+echo "A soma de A + B = ".($valorA + $valorB);
+//Subtração
+echo "<br> A subtração de A - B = ".($valorA - $valorB);
+//Multiplicação
+echo "<br> A multiplicação de A * B = ".($valorA * $valorB);
+//Divisão
+echo "<br> A divisão de A / B = ".($valorA / $valorB);
+//Incremento
+echo "<br> O incremento de A ++= ".(++$valorA);
+//Decremento
+echo "<br> O decremento de A --= ".(--$valorA);
+//Decremento sobre a variavel decrementada anteriormente
+echo "<br> O decremento de A --= ".(--$valorA);
