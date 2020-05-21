87d6fcff53e13ab8727a4254129da3d374180a9a
Leandro Costa <ltitreinamento@gmail.com>
2020-05-21 09:36:08 -0300

    Inclusão dos Operadores de Comparação no arquivo operadores.php

--- operadores.php
+++ operadores.php
@@ -15,3 +15,26 @@ echo "<br> O incremento de A ++= ".(++$valorA);
 echo "<br> O decremento de A --= ".(--$valorA);
 //Decremento sobre a variavel decrementada anteriormente
 echo "<br> O decremento de A --= ".(--$valorA);
+echo "<hr>";
+
+$compA = 8;
+$compB = 8;
+
+//Maior que
+echo "A > B, resultado = ".($compA>$compB);
+//Menor que
+echo "<br>A < B, resultado = ".($compA<$compB);
+//Maior ou igual
+echo "<br>A >= B, resultado = ".($compA>=$compB);
+//Menor ou igual
+echo "<br>A <= B, resultado = ".($compA<=$compB);
+//Igualdade
+echo "<br>A == B, resultado = ".($compA==$compB);
+//Diferença
+echo "<br>A != B, resultado = ".($compA!=$compB);
+//Diferença
+echo "<br>A <> B, resultado = ".($compA<>$compB);
+//Igual e do mesmo tipo
+echo "<br>A === B, resultado = ".($compA===$compB);
+//Diferente e do mesmo tipo
+echo "<br>A !== B, resultado = ".($compA!==$compB);
